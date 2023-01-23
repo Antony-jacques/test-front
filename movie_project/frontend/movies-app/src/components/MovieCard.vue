6e20073490d98241eaf3f4aa032cc64bad5e6d07
Antony <antony.jacques@laposte.net>
2023-01-23 19:38:30 +0100

    toggle details visibility

--- movie_project/frontend/movies-app/src/components/MovieCard.vue
+++ movie_project/frontend/movies-app/src/components/MovieCard.vue
@@ -1,40 +1,51 @@
 <template>
-<div class="card">
+  <div class="card" @click="toggleVisibility">
     <div class="title">{{ movie.title }}</div>
-    <div class="description">{{ movie.description }}</div>
-    <div class="actors" v-for="(actor, index) in getFeaturedActors" :key=index>{{ actor.first_name }} - {{ actor.last_name }}</div>
-    
-</div>
-
+    <div class="movie-details" v-if="visible">
+      <div class="description">{{ movie.description }}</div>
+      <div
+        class="actors"
+        v-for="(actor, index) in getFeaturedActors"
+        :key="index"
+      >{{ actor.first_name }} - {{ actor.last_name }}</div>
+    </div>
+  </div>
 </template>
 <script>
-    export default {
-        name: 'MovieCard',
-        props: {
-            movie: Object
-        },
-        computed: {
-             getActors() {
-                 return this.$store.state.actors.results
-             },
-             getFeaturedActors(){
-                 let featuredActors = []
-                 this.getActors.forEach((actor) => {
-                     if(this.movie.actors.includes(actor.id)) {
-                         featuredActors.push(actor)
-                     }
-                 })
-                 return featuredActors
-             }
-         },
-         mounted(){
-
-         }
+export default {
+  name: "MovieCard",
+  props: {
+    movie: Object
+  },
+  data(){
+      return {
+          visible: false
+      }
+  },
+  methods: {
+      toggleVisibility: function() {
+          this.visible = !this.visible
+      }
+  },
+  computed: {
+    getActors() {
+      return this.$store.state.actors.results;
+    },
+    getFeaturedActors() {
+      let featuredActors = [];
+      this.getActors.forEach(actor => {
+        if (this.movie.actors.includes(actor.id)) {
+          featuredActors.push(actor);
+        }
+      });
+      return featuredActors;
     }
+  },
+  mounted() {}
+};
 </script>
 <style scoped>
-    .card {
-        border: solid 1px;
-    }
-
+.card {
+  border: solid 1px;
+}
 </style>
